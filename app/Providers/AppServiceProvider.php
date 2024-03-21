c552149666240c07834a65bd05097e4719a21112
EC2 Default User <ec2-user@ip-10-0-1-65.ap-northeast-1.compute.internal>
2024-03-21 14:35:37 +0000

    provider追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,16 @@ namespace App\Providers;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Cashier\Cashier;
+use App\Services\easymarket\AuthService\AuthService;
+use App\Services\easymarket\AuthService\AuthServiceInterface;
+use App\Services\easymarket\ImageService\ImageService;
+use App\Services\easymarket\ImageService\ImageServiceInterface;
+use App\Services\easymarket\UserService\UserService;
+use App\Services\easymarket\UserService\UserServiceInterface;
+use App\Services\easymarket\ItemService\ItemService;
+use App\Services\easymarket\ItemService\ItemServiceInterface;
+//use ServiceeasymarketServiceProvider
+use App\Providers\ServiceeasymarketServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->bind(AuthServiceInterface::class, AuthService::class);
+        $this->app->bind(ImageServiceInterface::class, ImageService::class);
+        $this->app->bind(UserServiceInterface::class, UserService::class);
+
         // Cashier::ignoreMigrations();
     }
 
--- app/Providers/ServiceeasymarketServiceProvider.php
+++ app/Providers/ServiceeasymarketServiceProvider.php
@@ -18,10 +18,6 @@ class ServiceeasymarketServiceProvider extends ServiceProvider
             \App\Services\easymarket\AuthService\AuthService::class
         );
         $this->app->bind(
-            \App\Services\easymarket\DealService\DealServiceInterface::class,
-            \App\Services\easymarket\DealService\DealService::class
-        );
-        $this->app->bind(
             \App\Services\easymarket\ImageService\ImageServiceInterface::class,
             \App\Services\easymarket\ImageService\ImageService::class
         );
@@ -29,22 +25,6 @@ class ServiceeasymarketServiceProvider extends ServiceProvider
             \App\Services\easymarket\UserService\UserServiceInterface::class,
             \App\Services\easymarket\UserService\UserService::class
         );
-        $this->app->bind(
-            \App\Services\easymarket\ProductService\ProductServiceInterface::class,
-            \App\Services\easymarket\ProductService\ProductService::class
-        );
-        // envがtestingか、services.stripe.secretが設定されていない場合はStripeServiceのモックを返す
-        if (app()->environment('testing') || empty(config('services.stripe.secret'))) {
-            $this->app->bind(
-                \App\Services\easymarket\StripeService\StripeServiceInterface::class,
-                \App\Services\easymarket\StripeService\StripeServiceMock::class
-            );
-        } else {
-            $this->app->bind(
-                \App\Services\easymarket\StripeService\StripeServiceInterface::class,
-                \App\Services\easymarket\StripeService\StripeService::class
-            );
-        }        
     }
 
     /**
